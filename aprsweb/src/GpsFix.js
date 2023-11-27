b5fd4c25ebfa97cf8f50ad14517eafc0351654ff
Andy Oakley <andy@andyoakley.com>
2023-11-26 17:01:39 -0800

    Pass params in correct order

--- aprsweb/src/GpsFix.js
+++ aprsweb/src/GpsFix.js
@@ -20,7 +20,7 @@ export default function GpsFix() {
     const lat = (parts[3]==="N"?1:-1) * (parseFloat(lats.substring(0, 2)) + (parseFloat(lats.substring(2))/60.0))
     const lon = (parts[3]==="E"?1:-1) * (parseFloat(lons.substring(0, 3)) + (parseFloat(lats.substring(3))/60.0))
     
-    const mgrs_pos = forward([lat, lon])
+    const mgrs_pos = forward([lon, lat])
 
     return (<>
         <div>{lat.toFixed(3)},{lon.toFixed(3)} {mgrs_pos} ({parts[7]}🛰)</div>
